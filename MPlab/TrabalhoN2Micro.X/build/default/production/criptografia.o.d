5190183c3221c2c4ca6f644274002867c500ce39
alefemacedo <almacedobr2@gmail.com>
2018-06-15 18:13:55 -0300

    ciontinue

--- MPlab/TrabalhoN2Micro.X/build/default/production/criptografia.o.d
+++ MPlab/TrabalhoN2Micro.X/build/default/production/criptografia.o.d
@@ -1,4 +1,4 @@
 build/default/production/criptografia.o:  \
-	C:/Users/Alexsander/Downloads/periodo\ 7/Microcontrolador/trabalho-micro/MPlab/TrabalhoN2Micro.X/criptografia.c  \
-	C:/Users/Alexsander/Downloads/periodo\ 7/Microcontrolador/trabalho-micro/MPlab/TrabalhoN2Micro.X/stdint.h  \
+	C:/Users/aluno/Desktop/David/trabalho-micro/MPlab/TrabalhoN2Micro.X/criptografia.c  \
+	C:/Users/aluno/Desktop/David/trabalho-micro/MPlab/TrabalhoN2Micro.X/stdint.h  \
 
--- MPlab/TrabalhoN2Micro.X/build/default/production/main.o.d
+++ MPlab/TrabalhoN2Micro.X/build/default/production/main.o.d
@@ -1,6 +1,6 @@
 build/default/production/main.o:  \
-	C:/Users/Alexsander/Downloads/periodo\ 7/Microcontrolador/trabalho-micro/MPlab/TrabalhoN2Micro.X/main.c  \
-	C:/Users/Alexsander/Downloads/periodo\ 7/Microcontrolador/trabalho-micro/MPlab/TrabalhoN2Micro.X/Lcd_8bits.H  \
-	C:/Users/Alexsander/Downloads/periodo\ 7/Microcontrolador/trabalho-micro/MPlab/TrabalhoN2Micro.X/users.H  \
-	C:/Users/Alexsander/Downloads/periodo\ 7/Microcontrolador/trabalho-micro/MPlab/TrabalhoN2Micro.X/MCC18_18F4550.h  \
+	C:/Users/aluno/Desktop/David/trabalho-micro/MPlab/TrabalhoN2Micro.X/main.c  \
+	C:/Users/aluno/Desktop/David/trabalho-micro/MPlab/TrabalhoN2Micro.X/Lcd_8bits.H  \
+	C:/Users/aluno/Desktop/David/trabalho-micro/MPlab/TrabalhoN2Micro.X/users.H  \
+	C:/Users/aluno/Desktop/David/trabalho-micro/MPlab/TrabalhoN2Micro.X/MCC18_18F4550.h  \
 
